ab48ef0020648b7d36b09ab9aaaa189ca2387b19
hutamatr <hutamatr@gmail.com>
2022-05-24 02:39:36 +0700

    Make form validation in SimpleInput.js

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,48 @@
+import React, { useState } from "react";
+
 const SimpleInput = (props) => {
+  // Form Input Using useState
+  const [formInput, setFormInput] = useState("");
+  const [isFormTouched, setIsFormTouched] = useState(false);
+
+  const isFormValid = formInput.trim() !== "";
+  const nameFormIsInvalid = !isFormValid && isFormTouched;
+
+  const formInputHandler = (event) => {
+    setFormInput(event.target.value);
+  };
+
+  const formBlurHandler = () => {
+    setIsFormTouched(true);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setIsFormTouched(true);
+
+    if (!isFormValid) {
+      return;
+    }
+    // formInputRef.current.value = ""; // NOT IDEAL FOR REACT
+    setFormInput("");
+    setIsFormTouched(false);
+  };
+
+  const formValid = nameFormIsInvalid ? "form-control invalid" : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={formValid}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={formInputHandler}
+          onBlur={formBlurHandler}
+          value={formInput}
+        />
+        {nameFormIsInvalid && <p className="error-text">Please Input Name!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
